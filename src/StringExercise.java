948d98e30d4f832bf86a62264647482628b37147
Julia Contreras <juliacontreras483@gmail.com>
2020-06-06 23:23:37 -0500

    string exer

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,24 @@
+//public class StringExercise {
+//}
+
 public class StringExercise {
+    public static void main(String[] args) {
+        String first = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(first);
+
+        String second = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(second);
+
+        String third = "In windows, the main drive is usually C:\\";
+        System.out.println(third);
+
+        String fourth = "I can do backslashes \\, double backslashes \\\\,\n"+
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(fourth);
+    }
 }
+
 
 
 //    String Basics.
