2db5a430e4a62fbe7502abe570c29fc1ef33e58f
Julia Contreras <juliacontreras483@gmail.com>
2020-06-08 11:48:37 -0500

    test

---
+++ src/TestLecture.java
@@ -0,0 +1,11 @@
+public class TestLecture {
+
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+//        invoke our methods
+        helloWorld();
+    }
+
+}
