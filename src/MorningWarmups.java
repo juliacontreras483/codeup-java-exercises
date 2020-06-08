7db21522ea790feb6c3dd04c31755972f8f3f787
Julia Contreras <juliacontreras483@gmail.com>
2020-06-08 09:14:51 -0500

    morning warmup

---
+++ src/MorningWarmups.java
@@ -0,0 +1,10 @@
+public class MorningWarmups {
+    public static void main(String[] args){
+        for(int i = 0; i <= 50; i += 5){
+            System.out.println(i);
+        }
+    }
+}
+
+// creat a java class named MorningWarmups. In the main method, creat a loop that prints out numbers 1-50, counting by 5's.
+
