8d5fe66d5207cd09c99ffe5b084cd0f024fe3899
Julia Contreras <juliacontreras483@gmail.com>
2020-06-16 13:44:13 -0500

    mini abstract exer

--- src/Abstract.java
+++ src/Abstract.java
@@ -1,2 +1,15 @@
 public class Abstract {
 }
+
+
+//#### Mini-Exercise 2 (Abstract Classes)
+//        - Create an abstract class called Appliance with the following properties:
+//        - package-private brand;
+//        - package-private color;
+//        - an empty constructor and a constructor that sets the properties.
+//        - getters and setters for properties.
+//        - an abstract method named makeSound() - when implemented, prints the sound the appliance makes
+//        - an abstract method named doWork()- when implemented, prints what the appliance does
+//        - Create a concrete class of Refrigerator that extends Appliance. Implement all abstract methods.
+//        - Create a concrete class of Blender that extends Appliance. Implement all abstract methods.
+//        - Come up with your own concrete class to extend Appliance and implement abstract methods.
