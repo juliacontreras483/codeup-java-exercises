c6a9d1b9551da8a93d9578e112c3d1a994be60c2
Julia Contreras <juliacontreras483@gmail.com>
2020-06-17 23:54:18 -0500

    work on bonuses for arrays created files to start

---
+++ src/arraysLecture/BonusGroceryList.java
@@ -0,0 +1,16 @@
+package arraysLecture;
+
+public class BonusGroceryList {
+}
+//# Extra Array Exercises
+//        ​
+//        1. Create a command line grocery list maker
+//        ​
+//        - before prompting the user to enter each item, ask how many items will be
+//        entered
+//        - for each item, the user should be prompted to enter in the name, quantity,
+//        and category of each grocery item
+//        - Create a `GroceryItem` class from which to instantiate `GroceryItem`
+//        objects to store in an array
+//        - once the user has entered all items, print out all items grouped by
+//        category and alphabetized in each group​
