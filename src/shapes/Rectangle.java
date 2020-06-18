21269cab2347e16f376f693463633e777639ec1d
Julia Contreras <juliacontreras483@gmail.com>
2020-06-18 09:42:25 -0500

    reviewing walkthrough on exer

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,20 +1,44 @@
 package shapes;
 
-public class Rectangle {
-    protected double length;
-    protected double width;
+//public class Rectangle {
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getPerimeter(){
+//        return (2 * length) + (2 * width);
+//    }
+//
+//    public double getArea(){
+//        return length * width;
+//
+//    }
+//}
 
-    public Rectangle(double length, double width){
-        this.length = length;
-        this.width = width;
+public class Rectangle extends Quadrilateral implements Measurable {
+    @Override
+    public void setLength(double 1){
+        this.length = 1;
     }
 
-    public double getPerimeter(){
-        return (2 * length) + (2 * width);
+    @Override
+    public void setWidth(double w){
+        this.width = w;
     }
 
-    public double getArea(){
-        return length * width;
-
+    @Override
+    public void getPerimeter(){
+        this.eidth = w;
     }
+
+    @Override
+    public void getArea(double w){
+        this.eidth = w;
+    }
+
 }
+
