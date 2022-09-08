006e04deb6d054ec65f555d2ff6df15555547f03
Julia Contreras <juliacontreras483@gmail.com>
2022-09-08 17:43:42 -0500

    practice java

---
+++ src/util/Brooke.java
@@ -0,0 +1,16 @@
+package util;
+
+//package util;
+
+//import java.util.*;
+
+        import java.util.Scanner;
+
+public class Brooke {
+    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+        String S = "How are you";
+        System.out.println(" \"" + S + "\"" );
+        System.out.println((char)34);
+    }
+}
